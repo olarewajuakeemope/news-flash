03818eafb3e38ac31d9d7554c62e57f935202066
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-05-16 21:05:59 +0100

    setup(dependencies):install flux, axios and semantic import components to dashboardcontainer and install request dependencies [Finishes #]

--- src/components/DashboardContainer.jsx
+++ src/components/DashboardContainer.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import * as firebase from 'firebase';
+import DashboardApp from './DashboardApp';
+import LoadingSourcesModal from './LoadingSourcesModal';
 
 
-/**
- * This Component houses the DashboardApp and LoadingSourceModal components.
- * @return {ReactComponent|null|false} - A Component for DOM rendering.
- */
+
 export default function DashboardContainer() {
     const auth = firebase.auth();
     auth.onAuthStateChanged(user => {
      if (user) {
          console.log(user.uid);
      } else {
-         console.log('no user');
+         window.location.replace('/');
      }
     });
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+       <div>
+        <div className="col-lg-2 col-md-1 col-sm-0" />
+         <LoadingSourcesModal />
+         <DashboardApp />
+        <div className="col-lg-2 col-md-1 col-sm-0" />
+       </div>
   );
 }
